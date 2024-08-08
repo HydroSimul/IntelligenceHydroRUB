2a7dee2d5e16ff69f79bc81e633c323c41df203b
Kan Lei <kan.lei@ruhr-uni-bochum.de>
2024-08-08 11:06:46 +0200

    split inflow into every continent

--- R/basin_WGAP3.R
+++ R/basin_WGAP3.R
@@ -1,12 +1,40 @@
 #' Get the basin of one Cell with GCRC Number
 #'
 #' @param idx_Continent (string) Index of continents from ("eu", "af","as","au","na","sa")
-#' @param num_GCRC (int) GCRC number
-#' @importFrom terra aggregate
+#' @param num_GCRC (int / vector of int) GCRC number/-s
+#' @importFrom terra aggregate vect
 #' @export
 basin_GCRC <- function(idx_Continent, num_GCRC) {
-  vect_Cell <- lst_Mama_Cell[[idx_Continent]] |> vect()
-  lst_Inflow_GCRC <- c(lst_Inflow_GCRC1, lst_Inflow_GCRC2, lst_Inflow_GCRC3)
-  aggregate(vect_Cell[lst_Inflow_GCRC[[idx_Continent]][[num_GCRC]]])
+
+  vect_Cell <- switch(idx_Continent,
+                      eu = vect_Mama_Cell_eu,
+                      af = vect_Mama_Cell_af,
+                      as = vect_Mama_Cell_as,
+                      au = vect_Mama_Cell_au,
+                      na = vect_Mama_Cell_na,
+                      sa = vect_Mama_Cell_sa,
+                      stop('Invalid continent, make sure in ("eu", "af","as","au","na","sa")'))
+  int_Inflow_GCRC <- switch(idx_Continent,
+                      eu = int_Inflow_GCRC_eu,
+                      af = int_Inflow_GCRC_af,
+                      as = int_Inflow_GCRC_as,
+                      au = int_Inflow_GCRC_au,
+                      na = int_Inflow_GCRC_na,
+                      sa = int_Inflow_GCRC_sa,
+                      stop('Invalid continent, make sure in ("eu", "af","as","au","na","sa")'))
+
+  vect_Cell <- vect(vect_Cell)
+
+  vect_Basin <- vect()
+
+  for (i in num_GCRC) {
+    vect_Basin <- rbind(vect_Basin, aggregate(vect_Cell[int_Inflow_GCRC[[i]]]))
+  }
+
+
+  crs(vect_Basin) <- "EPSG:4326"
+  vect_Basin$GCRC <- num_GCRC
+  vect_Basin$Continent <- idx_Continent
+  vect_Basin
 }
 
